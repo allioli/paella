00aec025649ef9cab0741eb7602fb95ab9e42900
Xavi <xavi@example.com>
2024-10-03 13:16:12 +0200

    edit common specs

--- apitester/restassured-java/src/test/java/io/github/allioli/bookstore/specs/BookstoreSpecs.java
+++ apitester/restassured-java/src/test/java/io/github/allioli/bookstore/specs/BookstoreSpecs.java
@@ -2,6 +2,7 @@ package io.github.allioli.bookstore.specs;
 
 import io.restassured.authentication.OAuth2Scheme;
 import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.filter.log.LogDetail;
 import io.restassured.http.ContentType;
 import io.restassured.specification.RequestSpecification;
 
@@ -12,16 +13,17 @@ public class BookstoreSpecs {
         return new RequestSpecBuilder()
                 .addHeader("Content-Type", ContentType.JSON.toString())
                 .addHeader("Accept", ContentType.JSON.toString())
-                .build();
+                .build()
+                .log().ifValidationFails();
     }
 
     public static RequestSpecification getAuthRequestSpec(String token) {
 
         // Tried alternatively with setAuth() passing in OAuth2Scheme, but it triggers issue described
-        // in https://stackoverflow.com/questions/51533815/getting-error-classnotfoundexception-even-after-scribe-for-oauth-in-restassure
+        // in https://github.com/rest-assured/rest-assured/issues/884
 
         return new RequestSpecBuilder()
-                .addHeader("Authorization", "Bearer " + token)
-                .build();
+                .build()
+                .auth().oauth2(token);
     }
 }
